d78b7b8728838f342f71673bc35964d0dc72155b
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2016-05-23 13:37:02 -0300

    Alteração do init

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_info_tab do
   name 'Redmine Info Tab plugin'
   author 'Marcus Sacramento'
-  description 'Este Plugin permite que sejam exibidas informações em uma aba exclusiva, sendo configurável para permitir o uso de html'
+  description ''
   version '1.1.0'
   url 'https://github.com/marcus-sacramento-redmine/redmine_info_tab'
   author_url 'https://github.com/MarcuSacramento'
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_info_tab do
   
     menu :top_menu, :redmine_info_tab, { :controller => 'redmine_info_tab', :action => 'index' }, :caption => :info_tab_menu
      settings :default => {
-    :information_html => "<h3>Vá até as configurações e Altere o plugin.</h3></ p> Aceita formatação HTML",
+    :information_html => ''
   }, :partial => 'redmine_info_tab/redmine_info_tab_settings'
   
   
